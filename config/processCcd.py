e32e580a19189e235c2948381921c198bb6f7eac
Dan <danjampro@sky.com>
2021-05-05 10:47:39 +1000

    config

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,30 +1,37 @@
-"""
-Huntsman-specific overrides for ProcessCcdTask.
+""" Huntsman overrides for ProcessCcdTask.
 
-ProcessCcd runs a lot of processes, but they are split into three broad sections:
+ProcessCcd runs a lot of subtasks, but they are split into three broad sections:
 - ISR (instrument signature removal);
 - Image Characterisation (background subtraction, PSF modelling, CR repair);
 - Image Calibration (astrometric and photometric calibration).
-Subsequently, there are a **huge** number of config parameters that one can
-adjust for processCcd. To keep things a little tidier, I like to split the
- processCcd's config parameters into three other config files corresponding to
-  each of the above three sections.
 """
 import os.path
 from lsst.utils import getPackageDir
 
-#Grab the path to this config directory
+from huntsman.drp.lsst.tasks.characterizeImage import HuntsmanCharacterizeImageTask
+
 configDir = os.path.join(getPackageDir("obs_huntsman"), "config")
 
-# Load ISR configurations
+# ~~~~~ ISR subtask ~~~~
+
 config.isr.load(os.path.join(configDir, "isr.py"))
 config.isr.doBias = True
 config.isr.doFlat = True
 config.isr.doDark = True
 
-# Characterise
+# ~~~~~ Characterise image subtask ~~~~
+
+# Load default config
 config.charImage.load(os.path.join(configDir, "characterise.py"))
 
-# Load Calibrate configurations
+# Retarget this subtask to use Huntsman override
+config.charImage.retarget(HuntsmanCharacterizeImageTask)
+
+# Yes we want the outputs to be written to file
+config.charImage.doWrite = True
+config.charImage.doWriteExposure = False
+
+# ~~~~~ Calibrate subtask ~~~~
+
 config.doCalibrate = True
 config.calibrate.load(os.path.join(configDir, "calibrate.py"))
